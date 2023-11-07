456c5c7e89ca2171c9664b2bd5e8817830d96b26
Lucas Carturani <lucascarturani@gmail.com>
2023-11-06 22:05:10 -0300

    Desafio Finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone("(47)99999-9999", "Iphone 14 Pro Max", "351344358593116", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Subway Surfers");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("(47)98888-8888", "Nokia C22", "352939271143896", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Zombie Tsunami");
